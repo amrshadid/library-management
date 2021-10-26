9cc3c6a017b74d25e9a1d0f8983b8439c7efbd98
Shadid <amrshadid>
2021-10-26 13:17:57 -0700

    add payment method

--- checkout/views.py
+++ checkout/views.py
@@ -89,6 +89,7 @@ class Save_stripe_info(APIView):
             customer = stripe.Customer.create(
 
                 email=email, 
+                payment_method=payment_method_id,
                 invoice_settings={
                     'default_payment_method': payment_method_id
                 }
