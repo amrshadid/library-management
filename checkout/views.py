85a4a96c8b14bfa77acea645d69f80327f0d0d91
Shadid <amrshadid>
2021-10-27 15:35:57 -0700

    remove currency

--- checkout/views.py
+++ checkout/views.py
@@ -90,7 +90,6 @@ class Save_stripe_info(APIView):
 
                 email=email, 
                 payment_method=payment_method_id,
-                currency="usd",
                 invoice_settings={
                     'default_payment_method': payment_method_id
                 }
